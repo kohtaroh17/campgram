e10666e6b96af76eea2253d05a08297ac39f915b
kohtaroh17 <117917719+kohtaroh17@users.noreply.github.com>
2023-04-25 12:40:14 +0000

    [Mod] rails_admin.js

--- app/javascript/packs/rails_admin.js
+++ app/javascript/packs/rails_admin.js
@@ -1,21 +1,21 @@
-import "rails_admin/src/rails_admin/base";
-import "../stylesheets/rails_admin.scss";
+// import "rails_admin/src/rails_admin/base";
+// import "../stylesheets/rails_admin.scss";
 
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
+// import Rails from "@rails/ujs"
+// import Turbolinks from "turbolinks"
+// import * as ActiveStorage from "@rails/activestorage"
+// import "channels"
 
-import jquery from "jquery";
-import "popper.js";
-import "bootstrap";
-import "../stylesheets/application";
-import "jquery-ui-dist";
+// import jquery from "jquery";
+// import "popper.js";
+// import "bootstrap";
+// import "../stylesheets/application";
+// import "jquery-ui-dist";
 
 
-Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
+// Rails.start()
+// Turbolinks.start()
+// ActiveStorage.start()
 
-require("@fortawesome/fontawesome-free/js/all")
+// require("@fortawesome/fontawesome-free/js/all")
 
